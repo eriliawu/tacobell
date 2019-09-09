01f56aaea663fb181dfa4a3e417b651ae7da9119
eriliawu <cloveiverson@126.com>
2019-09-09 17:05:02 -0400

    add figure for menu item changes by year

--- match-menustat.R
+++ match-menustat.R
@@ -7,6 +7,12 @@ current_warning <- getOption("warn")
 options(warn = -1)
 #options(warn = current_warning)
 
+library(ggplot2)
+#install.packages("dplyr")
+library(dplyr)
+#install.packages("wesanderson")
+library(wesanderson)
+
 ### read and clean data ----
 #read clean restaurant data
 menu <- read.csv("data/menustat_tacobell_nutrition_data.csv", stringsAsFactors = FALSE)
@@ -50,9 +56,31 @@ menu_all <- menu_all[, c(1:2, 21, 3:20)]
 write.csv(menu_all, "data/menustat/nutrition_info_all.csv", row.names = FALSE)
 rm(menu, new_menu, i, menu_all)
 
-# link menustat data to transactions ----
+### analyze data from menu stat ----
 menu <- read.csv("data/menustat/nutrition_info_all.csv", stringsAsFactors = FALSE)
 table(menu$category)
 table(menu$year)
 
-sample <- menu[grepl("Bean Burrito", menu$item_name), ]
+# menu items available, by year, by category
+trend <- count(menu[menu$year<=2015, ], year, category)
+cat <- factor(trend$category, levels=c("Sandwiches", "Entrees", "Pizza", "Burgers",
+                                       "Appetizers & Sides", "Fried Potatoes",
+                                       "Salads", "Toppings & Ingredients",
+                                       "Baked Goods", "Beverages", "Desserts"))
+ggplot(data=trend, aes(x=as.character(year), y=n, group=cat, fill=cat)) +
+      geom_area(size=0.5, color="white") +
+      labs(title="Changes in TB menu items over time",
+           x="Year", y="Number of menu items", fill="Category",
+           caption="Data source: MenuStat, http://menustat.org") +
+      scale_fill_brewer(palette="Set3") +
+      theme(plot.title=element_text(hjust=0.5, size=18),
+            plot.caption=element_text(hjust=0, face="italic"))
+ggsave("tables/tb-menu/menu-items.jpeg", width=20, height=10, unit="cm")
+
+# menu item calorie changes, do items overall become more caloric
+ggplot(data=menu, aes(x=calories, color=category)) +
+      geom_histogram(fill=category)
+
+### link menu stat to transaction data ----
+sample <- menu[menu$item_name=="Bean Burrito", ]
+
