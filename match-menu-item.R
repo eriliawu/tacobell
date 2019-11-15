592bb63b32bbe712d2f0ab35bffad030950b4959
eriliawu <cloveiverson@126.com>
2019-11-15 16:45:57 -0500

    implement jero distance

--- match-menu-item.R
+++ match-menu-item.R
@@ -6,6 +6,12 @@ current_warning <- getOption("warn")
 options(warn = -1)
 #options(warn = current_warning)
 
+#install.packages("fuzzyjoin")
+library(fuzzyjoin)
+library(dplyr)
+#install.packages("stringdist")
+library(stringdist)
+
 # read product data ----
 product <- read.csv("data/from-bigpurple/product_dim.csv",
                     sep = ";", header = FALSE, quote = "\"'",
@@ -31,15 +37,18 @@ rm(group)
 table(product$group)
 
 # based on product group and product description
-# drop other YUM brand products, N/A group, non-food
+# drop other YUM brand products
 product <- product[!grepl("AW", product$group)&!grepl("BYB", product$group)&!grepl("KFC", product$group)&!grepl("LJS", product$group)&!grepl("PH", product$group)&!grepl("PIZZA HUT", product$group)&product$group!="KRYSTAL"&product$group!="ICBIY (YOGURT)"&product$group!="TCBY (YOGURT)", ]
-product <- product[!grepl("AWR", product$product)&!grepl("AW,", product$product)&!grepl("BYB", product$product)&!grepl("KFC", product$product)&!grepl("LJS", product$product)&!grepl("PH", product$product)&!grepl("PIZZA HUT", product$product)&!grepl("TCBY", product$product)&!grepl("ICBIY", product$product)&!grepl("KRYSTAL", product$product), ]
+product <- product[!grepl("AWR", product$product)&!grepl("AW,", product$product)&!grepl("AW ", product$product)&!grepl("BYB", product$product)&!grepl("KFC", product$product)&!grepl("LJS", product$product)&!grepl("PH", product$product)&!grepl("PIZZA HUT", product$product)&!grepl("TCBY", product$product)&!grepl("ICBIY", product$product)&!grepl("KRYSTAL", product$product), ]
+length(unique(product$product))
 
 # drop non-descriptive items
-product <- product[product$group!="COMBOS"&!grepl("* NEW PRODCT ADDED BY", product$product)&!grepl("COMBO", product$product)&!grepl("FRANCHISE LOCAL MENU", product$product)&product$product!="NEW ITEM", ]
+product <- product[product$group!="N/A"&product$group!="CFM MANAGER SPECIALS"&product$group!="COMBOS"&product$product!=""&!grepl("* NEW PRODCT ADDED BY", product$product)&!grepl("COMBO", product$product)&!grepl("FRANCHISE LOCAL MENU", product$product)&product$product!="NEW ITEM"&!grepl("SPECIAL PROMOTION", product$product), ]
+length(unique(product$product))
 
 # drop non-food items
-product <- product[product$group!="N/A"&product$group!="NON-FOOD SALES (PRE"&product$group!="CFM MANAGER SPECIALS", ]
+product <- product[product$group!="NON-FOOD SALES (PRE", ]
+length(unique(product$product))
 
 #length(unique(product$product))
 names(product)
@@ -55,9 +64,40 @@ menu <- menu[!duplicated(menu$item_name), c(1, 4:5)]
 length(unique(menu$item_name))
 #menu <- menu[menu$year<=2016, ]
 
-test <- merge(product, menu, by.x="product", by.y="item_name")
+### find exact matches ----
+test <- merge(product, menu, by.x="product", by.y="item_name") #64 exact matches
+rm(test)
 
+### fuzzy matching ----
+colnames(menu)[2] <- "product"
 
+start_time <- Sys.time()
+join <- stringdist_join(product, menu, 
+                by="product",
+                mode = "left",
+                ignore_case = FALSE, 
+                method = "jw", 
+                max_dist = 99, 
+                distance_col = "dist.jw") %>%
+      group_by(product.x) %>%
+      top_n(1, -dist.jw)
+end_time <- Sys.time()
+end_time - start_time
+rm(start_time, end_time)
+
+names(join)
+join <- join[, c(1, 4, 6, 2, 5, 3)]
+colnames(join)[1:2] <- c("product.tb", "product.menustat")
+join <- join[order(join$dist.jw, join$product.tb), ]
+
+### analyze matching results ----
+length(join$dist.jw[join$dist.jw==0])
+jpeg("tables/product-matching/jw-distance-distribution.jpeg",
+     width=600, height=400, quality=100)
+hist(join$dist.jw, breaks=50,
+     xlab="Distance",
+     main="Distribution of distances")
+dev.off()
 
 
 
