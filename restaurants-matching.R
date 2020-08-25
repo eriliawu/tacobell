48154bcc985b1e4bfdd4b4e17b2e8cdb154af483
E.Wu <cloveiverson@126.com>
2020-08-25 17:10:23 -0400

    geocode census tract from lon/lat, add ML timeline to cities and states

--- restaurants-matching.R
+++ restaurants-matching.R
@@ -8,7 +8,121 @@ options(warn = -1)
 #options(warn = current_warning)
 
 ### install and load packages ----
-install.packages("tidyverse")
 library(dplyr)
 library(ggplot2)
 library(tidyverse)
+#install.packages("tigris") #geocoding, add census tract number to geo coordinates
+#install.packages("sf")
+#install.packages("tidycensus") #get census tract boundaries
+library(tigris)
+library(sf)
+library(tidycensus)
+
+### import acs data from ipums ----
+acs <- read.csv("data/census-data/tract/nhgis0004_acs-for-matching/nhgis0004_ds201_20135_2013_tract.csv",
+                stringsAsFactors = FALSE)
+names(acs)
+acs$hsbelow <- rowSums(acs[, c(123:137)], na.rm = TRUE)
+acs$collegeup <- rowSums(acs[, c(143:146)])
+acs$under18 <- rowSums(acs[, c(40:43, 57:62)])
+acs$above65 <- rowSums(acs[, c(64:67, 81:86)])
+
+acs <- acs %>%
+  select(GISJOIN, STATE, STATEA, COUNTY, COUNTYA, TRACTA, UEEE002,
+         UEEE026, UEPE001, UEQE002, UEQE003, UEQE005, UEYE012,
+         UHDE001, UJAE001, hsbelow, collegeup, under18, above65) 
+colnames(acs)[1:15] <- c("geo", "state", "state_num", "county", "county_num", "tract_num",
+                       "male", "female", "total", "white", "black", "asian", "hisp",
+                       "median_income", "capita_income")
+sapply(acs, class)
+
+### add census tract number to restaurants ----
+restaurant <- read.csv("data/restaurants/analytic_restaurants.csv",
+                       stringsAsFactors = FALSE)
+summary(restaurant$lat)
+summary(restaurant$lon)
+names(restaurant)
+restaurant$tract_num <- substr(restaurant$tract_num, 2, 12) 
+#restaurant %>%
+#  filter(lat<0) %>%
+#  select(restid, address,lat, lon)
+#restaurant$lat[restaurant$restid==139763] <- 39.3409
+#restaurant$lon[restaurant$restid==139763] <- -105.0659
+#restaurant$lon[restaurant$restid==149611] <- -117.8847
+
+restaurant %>%
+  filter(lon>0) %>%
+  select(restid, address, open, close, lat, lon) # fill in lat/lon manually in csv file after google searches
+#restaurant$block_num <- apply(restaurant, 1, function(row) call_geolocator_latlon(row['lat'], row['lon']))
+#restaurant$tract_num <- substr(restaurant$block_num, 1, 11)
+#restaurant$tract_num <- paste0('"',restaurant$tract_num, '"') #forcing csv to read these as characters
+#restaurant$block_num <- paste0('"',restaurant$block_num, '"')
+#write.csv(restaurant, "data/restaurants/analytic_restaurants.csv", row.names = FALSE)
+#restaurant$tract_num <- substr(restaurant$tract_num, 2,12)
+#restaurant$block_num <- substr(restaurant$block_num, 2,16)
+
+acs$geo1 <- substr(acs$geo, 2,3)
+acs$geo2 <- substr(acs$geo, 5,7)
+acs$geo3 <- substr(acs$geo, 9,14)
+acs$geo <- paste0(acs$geo1, acs$geo2, acs$geo3)
+acs <- acs[, -c(3,5:6,20:22)]
+names(acs)
+colnames(acs)[1] <- "tract_num"
+#write.csv(acs, "data/census-data/tract/nhgis0004_acs-for-matching/nhgis0004_clean.csv",
+#          row.names = FALSE)
+acs[, c(4,5,7:10,13:16)] <- acs[, c(4,5,7:10,13:16)]/acs$total
+restaurant <- merge(restaurant, acs, by="tract_num") #lose 3 restaurants
+names(restaurant)
+restaurant <- restaurant %>%
+  select(tract_num:close, concept:drive_thru_type, ownership, city, county.x, male:above65)
+colnames(restaurant)[c(4,14)] <- c("state", "county")
+
+#change date to date format
+sapply(restaurant, class)
+restaurant <- cbind(restaurant[c(1:4, 9:27)], lapply(restaurant[c(5:8)], function(x) as.Date(x, "%m/%d/%Y")))
+
+### add timeline for menu labeling in city/state ----
+#C:\Users\wue04\NYU Langone Health\Elbel, Brian - Taco Bell labeling R01\PROPOSAL\Menu Labeling Legislation Research
+table(restaurant$city[restaurant$state=="NY"])
+
+restaurant$ml <- ifelse(restaurant$state=="NY"&
+                          (restaurant$county=="New York"|restaurant$county=="Kings"|
+                             restaurant$county=="Bronx"|restaurant$county=="Queens"|
+                             restaurant$county=="Richmond")&
+                          ((restaurant$year>=2008&restaurant$month>=5)|
+                             restaurant$year>=2009), 1,
+                        ifelse(restaurant$state=="WA"&restaurant$county=="King"&
+                                 restaurant$year>=2009, 1,
+                        ifelse(restaurant$state=="NY"&restaurant$county=="Albany"&
+                                 ((restaurant$year>=2010&restaurant$month>=2)|
+                                  restaurant$year>=2011), 1,
+                        ifelse(restaurant$state=="PA"&restaurant$city=="Philadelphia"&
+                                 restaurant$year>=2010, 1, 
+                        ifelse(restaurant$state=="CA"&(restaurant$city=="San Francisco"|
+                                  restaurant$county=="San Francisco")&
+                                 (restaurant$year>=2009|(restaurant$year==2008&restaurant$month>=10)), 1,
+                        ifelse(restaurant$state=="NY"&restaurant$county=="Westchester"&
+                                 (year>=2010|(year==2009&month>=5)), 1,
+                        ifelse(restaurant$state=="MD"&restaurant$county=="Montgomery"&
+                                 (year>=2011|(year==2010&month>=7)), 1,
+                        ifelse(restaurant$state=="OR"&restaurant$county=="Multnomah"&
+                                 (year>=2011|(year==2010&month>=7)), 1,
+                        ifelse(restaurant$state=="CA"&restaurant$year>=2011, 1,
+                        ifelse(restaurant$state=="MA"&(restaurant$year>=2011|
+                                  (restaurant$year==2010&restaurant$month>=11)), 1,
+                        ifelse(restaurant$state=="ME"&(restaurant$year>=2012|
+                                  (restaurant$year==2011&restaurant$month>=2)), 1,
+                        ifelse(restaurant$state=="NJ"&(restaurant$year>=2012|
+                                  (restaurant$year==2011&restaurant$month>=2)), 1,
+                        ifelse(restaurant$state=="OR"&restaurant$year>=2011, 1,
+                        ifelse(restaurant$state=="NY"&restaurant$county=="Ulster"&
+                                 (restaurant$year>=2010|(restaurant$year==2009&
+                                  restaurant$month>=10)),1,
+                        iflese(restaurant$state=="NY"&restaurant$county=="Nassau"&
+                                 (restaurant$year>=2010)|(restaurant$year==2009)&
+                                 restaurant$month>=11, 1,
+                        ifelse(restaurant$state=="VT"&(restaurant$year>=2013|
+                                (restaurant$year==2011&restaurant$month>=6)), 1, 0))))))))))))))))
+
+
+
