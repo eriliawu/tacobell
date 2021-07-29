cd0138d4cfc5e9dec555bcb67e4795cee7b275ec
E.Wu <cloveiverson@126.com>
2021-07-29 17:02:46 -0400

    painstakingly figuring out chaining

--- replicate-food-category.py
+++ replicate-food-category.py
@@ -14,7 +14,7 @@ product = pd.read_csv("data/from-bigpurple/product_dim.csv", sep=";", header=Non
 group = pd.read_csv('data/from-bigpurple/product_group_det.csv',sep=';',header=None,usecols=[0,2],
                     names=['dw_productgroup','group'])
 product = (pd.merge(product,group,on='dw_productgroup')
-            .query('product.str.contains("^(?!.*(NEW|PEPSI)).*$",na=False)',engine='python'))
+            .query('product.str.contains("^(?!.*(AWR| AW|AW,|AW |BYB|KFC|LJS|PH |PIZZA HUT|TCBY|ICBIY|KRYSTAL|* NEW PRODCT ADDED BY|COMBO|FRANCHISE LOCAL MENU|SPECIAL PROMOTION|NEW ITEM|TB I\'M ALL EARS|DO NOT ALTER THIS ITEM|BORDER SWEAT SHIRT|TB I\'M THINKING YOU ME|CFM DOWNLOAD 1|TB HELLO FRIEND|CANADA BATMAN CUP INDIVIDUAL|DELETED ITEM, DO NOT USE|CLEV INDIANS/TB BANDANNA 1.4|CFM DOWNLOAD 2|TB I\'M THINKING YOU ME DINNER|CANADA BATMAN CUP W/PURCHASE|TB HELLO FRIENDGC REFUND|TB EAT IN CAR)).*$",na=False)',engine='python'))
 """
 # or do the second part of the operation like this, which doesnt involve regular expression
 product = product[~product['product'].str.contains('NEW')]
