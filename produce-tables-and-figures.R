99a35dee7a6cf563ff394747066bbcf009d950ed
E.Wu <cloveiverson@126.com>
2021-09-01 23:22:54 -0400

    add table2, app fig3, app table1

--- produce-tables-and-figures.R
+++ produce-tables-and-figures.R
@@ -162,6 +162,7 @@ ggplot(data=trend,aes(x=month, y=calorie,color=as.character(group))) +
 
 ### fig3, diff in diff, by location ----
 trend <- NULL
+presum <- "treat:relative2.factor-4 + treat:relative2.factor-5 + treat:relative2.factor-6 + treat:relative2.factor-7 + treat:relative2.factor-8"
 for (p in c("ca","ma","or","suffolk")) {
   mod.factor <- plm(formula = calorie~treat*relative2.factor+as.factor(month),
                     data = matched%>%filter(((relative2>=-30&relative2<=-3)|(relative2>=2&relative2<=29))&match_place==p), 
@@ -218,7 +219,7 @@ ggplot() +
         legend.text=element_text(size=10), 
         plot.caption=element_text(hjust=0, vjust=-15, face="italic"),
         panel.grid.minor = element_blank())
-ggsave("manuscript/figures/fig2-diff-in-diff-by-loc.jpeg", dpi="retina")
+#ggsave("manuscript/figures/fig2-diff-in-diff-by-loc.jpeg", dpi="retina")
 rm(tmp1,tmp2,tmp,mod.factor,i,p,tidy_mod.factor,trend)
 
 ### table 3, diff in diff, overall and by location ----
@@ -409,7 +410,7 @@ restaurant <- read.csv("data/restaurants/analytic_restaurants.csv",
   mutate(tract_num=substr(tract_num,2,12))
 calorie <- merge(calorie,restaurant,by="restid",all.x = TRUE) %>% dplyr::select(-restid) %>%
   group_by(address,concept,ownership,monthno,tract_num,occasion) %>% summarise_all(sum)
-matched_use <- matched %>% filter(treat==1&relative2>=2&relative2<=11) %>% dplyr::select(address,tract_num,concept,ownership,monthno)
+matched_use <- matched %>% filter(treat==1&relative2>=2&relative2<=11) %>% dplyr::select(address,tract_num,concept,ownership,monthno,match_place)
 calorie <- merge(calorie,matched_use,by=c("address","monthno","concept","ownership","tract_num"))  %>%
   mutate(across(calorie:sodium, ~ ./count)) %>% dplyr::select(-count)
 
@@ -473,22 +474,245 @@ calorie <- merge(calorie,restaurant,by="restid",all.x = TRUE) %>% dplyr::select(
 calorie <- merge(calorie,matched_use,by=c("address","monthno","concept","ownership","tract_num"))  %>%
   mutate(across(calorie:sodium, ~ ./count)) %>% dplyr::select(-count)
 tmp <- calorie %>% dplyr::select(category:sodium) %>% group_by(category) %>% summarise_all(list(mean=mean,sd=sd)) %>%
-  mutate(cal=paste0(sprintf('%.0f',calorie_mean)," (",sprintf('%.0f',calorie_sd),")")) %>%
-  mutate(fat=paste0(sprintf('%.2f',fat_mean)," (",sprintf('%.2f',fat_sd),")")) %>%
-  mutate(carb=paste0(sprintf('%.0f',carb_mean)," (",sprintf('%.2f',carb_sd),")")) %>%
-  mutate(protein=paste0(sprintf('%.2f',protein_mean)," (",sprintf('%.2f',protein_sd),")")) %>%
-  mutate(sat_fat=paste0(sprintf('%.2f',sat_fat_mean)," (",sprintf('%.2f',sat_fat_sd),")")) %>%
-  mutate(sugar=paste0(sprintf('%.2f',sugar_mean)," (",sprintf('%.2f',sugar_sd),")")) %>%
-  mutate(fiber=paste0(sprintf('%.2f',fiber_mean)," (",sprintf('%.2f',fiber_sd),")")) %>%
-  mutate(sodium=paste0(sprintf('%.0f',sodium_mean)," (",sprintf('%.0f',sodium_sd),")")) %>%
+  mutate(cal=paste0(ifelse(calorie_mean>=100,sprintf('%.0f',calorie_mean),sprintf('%.2f',calorie_mean)),
+                    " (",ifelse(calorie_sd>=100,sprintf('%.0f',calorie_sd),sprintf('%.2f',calorie_sd)),")")) %>%
+  mutate(fat=paste0(ifelse(fat_mean>=100,sprintf('%.0f',fat_mean),sprintf('%.2f',fat_mean)),
+                    " (",ifelse(fat_sd>=100,sprintf('%.0f',fat_sd),sprintf('%.2f',fat_sd)),")")) %>%
+  mutate(carb=paste0(ifelse(carb_mean>=100,sprintf('%.0f',carb_mean),sprintf('%.2f',carb_mean)),
+                    " (",ifelse(carb_sd>=100,sprintf('%.0f',carb_sd),sprintf('%.2f',carb_sd)),")")) %>%
+  mutate(protein=paste0(ifelse(protein_mean>=100,sprintf('%.0f',protein_mean),sprintf('%.2f',protein_mean)),
+                    " (",ifelse(protein_sd>=100,sprintf('%.0f',protein_sd),sprintf('%.2f',protein_sd)),")")) %>%
+  mutate(sat_fat=paste0(ifelse(sat_fat_mean>=100,sprintf('%.0f',sat_fat_mean),sprintf('%.2f',sat_fat_mean)),
+                    " (",ifelse(sat_fat_sd>=100,sprintf('%.0f',sat_fat_sd),sprintf('%.2f',sat_fat_sd)),")")) %>%
+  mutate(sugar=paste0(ifelse(sugar_mean>=100,sprintf('%.0f',sugar_mean),sprintf('%.2f',sugar_mean)),
+                    " (",ifelse(sugar_sd>=100,sprintf('%.0f',sugar_sd),sprintf('%.2f',sugar_sd)),")")) %>%
+  mutate(fiber=paste0(ifelse(fiber_mean>=100,sprintf('%.0f',fiber_mean),sprintf('%.2f',fiber_mean)),
+                    " (",ifelse(fiber_sd>=100,sprintf('%.0f',fiber_sd),sprintf('%.2f',fiber_sd)),")")) %>%
+  mutate(sodium=paste0(ifelse(sodium_mean>=100,sprintf('%.0f',sodium_mean),sprintf('%.2f',sodium_mean)),
+                    " (",ifelse(sodium_sd>=100,sprintf('%.0f',sodium_sd),sprintf('%.2f',sodium_sd)),")")) %>%
   dplyr::select(-c(2:17))
-tmp <- tmp[c(2,1,3),]
-table2[1:3,] <- tmp[,2:9]
+tmp <- tmp[c(6,2,5,4,3,1),]
+table2[10:15,] <- tmp[,2:9]
+
+# panel 4, read by daypart data
+sample07q1 <- read.csv("data/from-bigpurple/mean-calorie-w-mod/by-restaurant-daypart-occasion/mean-calorie_restid_daypart_2007_Q1.csv",
+                       stringsAsFactors = FALSE,
+                       col.names = c("restid","monthno","occasion","daypart","calorie", "fat","carb",
+                                     "protein","sat_fat","sugar","fiber","sodium", "count"))
+sample07q1[, c(5:12)] <- sample07q1[, c(5:12)]/2
+calorie <- NULL
+for (i in 2007:2015) {
+  for (j in 1:4) {
+    tryCatch(
+      if((i==2007 & j==1)|(i==2015 & j==4)) {stop("file doesn't exist")} else
+      {
+        sample <- read.csv(paste0("data/from-bigpurple/mean-calorie-w-mod/by-restaurant-daypart-occasion/mean-calorie_restid_daypart_",
+                                  i,"_Q",j,".csv"),
+                           stringsAsFactors = FALSE,
+                           col.names=c("restid","monthno","occasion","daypart","calorie", "fat","carb",
+                                       "protein","sat_fat","sugar","fiber","sodium", "count"))
+        calorie <- rbind(calorie, sample)
+      }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")}
+    )
+  }
+}
+calorie <- rbind(calorie, sample07q1) %>% filter(occasion==2) %>% dplyr::select(-occasion)
+rm(sample, sample07q1, i, j)
+calorie <- merge(calorie,restaurant,by="restid",all.x = TRUE) %>% dplyr::select(-restid) %>%
+  group_by(address,concept,ownership,monthno,tract_num,daypart) %>% summarise_all(sum)
+calorie <- merge(calorie,matched_use,by=c("address","monthno","concept","ownership","tract_num"))  %>%
+  mutate(across(calorie:sodium, ~ ./count)) %>% dplyr::select(-count)
+tmp <- calorie %>% dplyr::select(daypart:sodium) %>% group_by(daypart) %>% summarise_all(list(mean=mean,sd=sd)) %>%
+  mutate(cal=paste0(ifelse(calorie_mean>=100,sprintf('%.0f',calorie_mean),sprintf('%.2f',calorie_mean)),
+                    " (",ifelse(calorie_sd>=100,sprintf('%.0f',calorie_sd),sprintf('%.2f',calorie_sd)),")")) %>%
+  mutate(fat=paste0(ifelse(fat_mean>=100,sprintf('%.0f',fat_mean),sprintf('%.2f',fat_mean)),
+                    " (",ifelse(fat_sd>=100,sprintf('%.0f',fat_sd),sprintf('%.2f',fat_sd)),")")) %>%
+  mutate(carb=paste0(ifelse(carb_mean>=100,sprintf('%.0f',carb_mean),sprintf('%.2f',carb_mean)),
+                     " (",ifelse(carb_sd>=100,sprintf('%.0f',carb_sd),sprintf('%.2f',carb_sd)),")")) %>%
+  mutate(protein=paste0(ifelse(protein_mean>=100,sprintf('%.0f',protein_mean),sprintf('%.2f',protein_mean)),
+                        " (",ifelse(protein_sd>=100,sprintf('%.0f',protein_sd),sprintf('%.2f',protein_sd)),")")) %>%
+  mutate(sat_fat=paste0(ifelse(sat_fat_mean>=100,sprintf('%.0f',sat_fat_mean),sprintf('%.2f',sat_fat_mean)),
+                        " (",ifelse(sat_fat_sd>=100,sprintf('%.0f',sat_fat_sd),sprintf('%.2f',sat_fat_sd)),")")) %>%
+  mutate(sugar=paste0(ifelse(sugar_mean>=100,sprintf('%.0f',sugar_mean),sprintf('%.2f',sugar_mean)),
+                      " (",ifelse(sugar_sd>=100,sprintf('%.0f',sugar_sd),sprintf('%.2f',sugar_sd)),")")) %>%
+  mutate(fiber=paste0(ifelse(fiber_mean>=100,sprintf('%.0f',fiber_mean),sprintf('%.2f',fiber_mean)),
+                      " (",ifelse(fiber_sd>=100,sprintf('%.0f',fiber_sd),sprintf('%.2f',fiber_sd)),")")) %>%
+  mutate(sodium=paste0(ifelse(sodium_mean>=100,sprintf('%.0f',sodium_mean),sprintf('%.2f',sodium_mean)),
+                       " (",ifelse(sodium_sd>=100,sprintf('%.0f',sodium_sd),sprintf('%.2f',sodium_sd)),")")) %>%
+  dplyr::select(-c(2:17))
+table2[16:21,] <- tmp[,2:9]
+table2$fiber <- gsub("-","",table2$fiber)
+table2 <- table2 %>%
+  add_row(cal="",.before=1) %>% add_row(cal="",.before=5) %>% add_row(cal="",.before=5) %>%
+  add_row(cal="",.before=13) %>% add_row(cal="",.before=13) %>%
+  add_row(cal="",.before=21) %>% add_row(cal="",.before=21) %>%
+  mutate_all(~replace(., is.na(.), ""))
+#write.csv(table2, "manuscript/tables/table2.csv")
+rm(tmp,table2,calorie,matched_use)
+
+### appendix figure 3, nutrient analysis, change in nutrient per transaction ----
+sample07q1 <- read.csv("data/from-bigpurple/mean-calorie-w-mod/by-restaurant-occasion/mean-calorie_restid_2007_Q1.csv",
+                       stringsAsFactors = FALSE,
+                       col.names = c("restid","monthno","occasion","calorie", "fat","carb",
+                                     "protein","sat_fat","sugar","fiber","sodium", "count"))
+sample07q1[, c(4:11)] <- sample07q1[, c(4:11)]/2
+calorie <- NULL
+for (i in 2007:2015) {
+  for (j in 1:4) {
+    tryCatch(
+      if((i==2007 & j==1)|(i==2015 & j==4)) {stop("file doesn't exist")} else
+      {
+        sample <- read.csv(paste0("data/from-bigpurple/mean-calorie-w-mod/by-restaurant-occasion/mean-calorie_restid_",
+                                  i,"_Q",j,".csv"),
+                           stringsAsFactors = FALSE,
+                           col.names=c("restid","monthno","occasion","calorie","fat","carb",
+                                       "protein","sat_fat","sugar","fiber","sodium", "count"))
+        calorie <- rbind(calorie, sample)
+      }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")}
+    )
+  }
+}
+calorie <- rbind(calorie, sample07q1)
+rm(sample, sample07q1, i, j)
+#merge with restaurants and aggregate by restaurant-month
+restaurant <- read.csv("data/restaurants/analytic_restaurants.csv",
+                       colClasses=c("integer","character",rep("NULL",7),"character",rep("NULL",3),"character",rep("NULL",15),"character")) %>%
+  mutate(concept=ifelse(concept=="TBC",1,0),ownership=ifelse(ownership=="COMPANY",1,0)) %>%
+  mutate(tract_num=substr(tract_num,2,12))
+calorie <- merge(calorie,restaurant,by="restid",all.x = TRUE) %>% dplyr::select(-restid) %>%
+  group_by(address,concept,ownership,monthno,tract_num,occasion) %>% summarise_all(sum)
+tmp <- matched %>% dplyr::select(id:month,relative2,relative2.factor,id_match)
+calorie <- merge(calorie,tmp,by=c("address","monthno","concept","ownership","tract_num")) %>%
+  mutate(across(calorie:sodium, ~ ./count)) %>% filter(occasion==2) %>% dplyr::select(-occasion) %>%
+  gather(key="nutrient",value="value","fat","carb","protein","sat_fat","sugar","fiber","sodium") %>%
+  mutate(nutrient=factor(nutrient,levels = c("fat","carb","protein","sat_fat","sugar","fiber","sodium")))
+
+trend <- NULL
+presum <- "treat:relative2.factor-4 + treat:relative2.factor-5 + treat:relative2.factor-6 + treat:relative2.factor-7 + treat:relative2.factor-8"
+for (i in c("fat","carb","protein","sat_fat","sugar","fiber","sodium")) {
+  mod.factor <- plm(formula = value~treat*relative2.factor+as.factor(month),
+                    data = calorie%>%filter(((relative2>=-30&relative2<=-3)|(relative2>=2&relative2<=29))&nutrient==i), 
+                    index = "id_match", weights = weights, model = "within")
+  tidy_mod.factor <- tidy(mod.factor)
+  tidy_mod.factor <- tidy_mod.factor %>%
+    dplyr::select(term,estimate,p.value) %>%
+    rename(month=term,coef.month=estimate,p=p.value) %>%
+    filter(!grepl("as.factor|calorie", month)) %>%
+    mutate(group=c(rep(0,55),rep(1,55))) %>%
+    add_row(month="-3",coef.month=0,group=0) %>%
+    add_row(month="-3",coef.month=0,group=1) %>%
+    mutate(month=as.integer(gsub("treat:relative2.factor|relative2.factor","",month))) %>%
+    mutate(month=ifelse(month>0,month+1,month)) %>%
+    arrange(group,month) %>%
+    mutate(diff = ifelse(group==1,coef.month,NA)) %>%
+    mutate(calorie=ifelse(group==0,coef.month,coef.month+coef.month[1:56]))
+  tmp1 <- tidy_mod.factor %>% 
+    filter(month>=-30&month<0&!is.na(diff)) %>% dplyr::select(month, diff) %>%
+    mutate(month = -month) %>% arrange(month) %>% mutate(pre_mean = sum(diff[1:6])/6)
+  tmp2 <- tidy_mod.factor %>% 
+    filter(month>=1&month<=30&!is.na(diff)) %>% dplyr::select(month, diff) %>%
+    arrange(month) %>% rename(post_mean = diff)
+  tmp1 <- merge(tmp1,tmp2,by="month") %>% group_by(month) %>% arrange(month) %>%
+    mutate(mean = post_mean - pre_mean) %>% dplyr::select(-diff)
+  tmp <- data.frame(matrix(data=0,nrow=28,ncol=1)) %>% setNames("p")
+  for (j in 4:29) {
+    tmp$p[j-1] <- linearHypothesis(mod.factor, paste0(presum," = 6*treat:relative2.factor",j))[2,4]
+  }
+  tmp1 <- cbind(tmp1, tmp)
+  tmp1$nutrient <- i
+  trend <- rbind(trend,tmp1)
+}
+
+summary(trend$mean) #[-82,56]
+ggplot() + 
+  geom_line(data=trend, aes(x=month, y=mean, color=nutrient)) + 
+  geom_point(data=trend%>%filter(p<0.05), aes(x=month, y=mean, color=nutrient),size=1) +
+  ggplot2::annotate(geom="label", x=6, y=-10, label="   p<0.05", size=3) + 
+  geom_point(aes(x=5.35,y=-10),color="black",size=1) +
+  coord_cartesian(expand = FALSE, clip = "off") + 
+  scale_y_continuous(limits=c(-90,60),breaks=seq(-90,60,10)) +
+  scale_x_continuous(breaks=seq(3,30,1)) +
+  labs(title="", x="Month", y="Difference-in-difference estimate", caption="") + 
+  scale_color_manual(name="Nutrient", labels=c("Total fat (g)","Carbohydrate (g)","Protein (g)","Saturated fat (g)","Sugar (g)","Fiber (g)","Sodium (mg)"),
+                     values=c("hotpink","olivedrab3","#13B0E4","orange","red","grey","black")) +
+  theme(plot.margin = unit(c(1, 1, 1, 1), "lines"),
+        plot.title = element_text(hjust = 0.5, size = 16), #position/size of title
+        axis.title.x = element_text(vjust=-1, size = 12), #vjust to adjust position of x-axis
+        axis.title.y = element_text(size = 12),
+        legend.text=element_text(size=10), 
+        plot.caption=element_text(hjust=0, vjust=-15, face="italic"),
+        panel.grid.minor = element_blank())
+#ggsave("manuscript/figures/appendix-fig3-diff-in-diff-nutrients.jpeg", dpi="retina")
+
+### appendix table 1, number of restaurants, by open time ----
+app_table1 <- data.frame(matrix(data=NA,ncol = 7,nrow = 14))
+colnames(app_table1)[1:7] <- c("loc","unmatched","matched_baseline","open12","open18","open24","open30")
+app_table1$loc <- c("trea_all","comp_all","ca","ca_comp","ma","ma_comp","or","or_comp",
+                    "mont","mont_comp","suffolk","suffolk_comp","king","king_comp")
+
+#get indicator for open 30 months post labeling
+tmp <- matched %>%
+  group_by(id, treat, match_place) %>%
+  filter(relative2<=29&relative2>=0) %>% mutate(n=n()) %>% mutate(after30=ifelse(n==30,1,0)) %>%
+  dplyr::select(id,treat,match_place,after30) %>%
+  distinct()
+matched <- merge(matched, tmp, by=c("id","treat","match_place"),all=TRUE)
+rm(tmp)
+
+#run the restaurants-matching-drive-thru.R script up to line 325
+restaurant <- restaurant %>%  mutate(id = group_indices(., address, tract_num, ownership, concept)) 
+length(unique(restaurant$id[restaurant$treat==1])) #1208
+length(unique(restaurant$id[restaurant$treat==0])) #5645
+app_table1$unmatched[1:2] <-c(1208,5645)
+for (i in c("ca","ma","or","mont","suffolk","king")) {
+  print(paste0(i,": ",length(unique(restaurant$id[restaurant$treat==1&restaurant$policy==i]))))
+  app_table1$unmatched[app_table1$loc==i] <- length(unique(restaurant$id[restaurant$treat==1&restaurant$policy==i]))
+}
+rm(i)
+
+#open ever
+length(unique(matched$id_match[matched$treat==1&matched$relative2==0])) #506
+length(unique(matched$id_match[matched$treat==0&matched$relative2==0])) #1518
+app_table1$matched_baseline[1:2] <-c(506,1518)
+for (i in c("ca","ma","or","mont","suffolk","king")) {
+  app_table1$matched_baseline[app_table1$loc==i] <- length(unique(matched$id_match[matched$treat==1&matched$match_place==i]))
+  app_table1$matched_baseline[app_table1$loc==paste0(i,"_comp")] <- length(unique(matched$id_match[matched$treat==0&matched$match_place==i]))
+  app_table1$open12[app_table1$loc==i] <- length(unique(matched$id_match[matched$treat==1&matched$match_place==i&matched$after12==1]))
+  app_table1$open12[app_table1$loc==paste0(i,"_comp")] <- length(unique(matched$id_match[matched$treat==0&matched$match_place==i&matched$after12==1]))
+  app_table1$open18[app_table1$loc==i] <- length(unique(matched$id_match[matched$treat==1&matched$match_place==i&matched$after18==1]))
+  app_table1$open18[app_table1$loc==paste0(i,"_comp")] <- length(unique(matched$id_match[matched$treat==0&matched$match_place==i&matched$after18==1]))
+  app_table1$open24[app_table1$loc==i] <- length(unique(matched$id_match[matched$treat==1&matched$match_place==i&matched$after24==1]))
+  app_table1$open24[app_table1$loc==paste0(i,"_comp")] <- length(unique(matched$id_match[matched$treat==0&matched$match_place==i&matched$after24==1]))
+  app_table1$open30[app_table1$loc==i] <- length(unique(matched$id_match[matched$treat==1&matched$match_place==i&matched$after30==1]))
+  app_table1$open30[app_table1$loc==paste0(i,"_comp")] <- length(unique(matched$id_match[matched$treat==0&matched$match_place==i&matched$after30==1]))
+}
+app_table1[1,4:7] <- app_table1[3,4:7]+app_table1[5,4:7]+app_table1[7,4:7]+app_table1[9,4:7]+app_table1[11,4:7]+app_table1[13,4:7]
+app_table1[2,4:7] <- app_table1[4,4:7]+app_table1[6,4:7]+app_table1[8,4:7]+app_table1[10,4:7]+app_table1[12,4:7]+app_table1[14,4:7]
+app_table1 <- app_table1 %>% mutate(open12=paste0(open12," (",sprintf('%.2f',open12/matched_baseline*100),"%)")) %>%
+  mutate(open18=paste0(open18," (",sprintf('%.2f',open18/matched_baseline*100),"%)")) %>%
+  mutate(open24=paste0(open24," (",sprintf('%.2f',open24/matched_baseline*100),"%)")) %>%
+  mutate(open30=paste0(open30," (",sprintf('%.2f',open30/matched_baseline*100),"%)")) %>%
+  mutate_all(~replace(., is.na(.), "-")) %>%
+  add_row(unmatched="",.before=3) %>% add_row(unmatched="",.before=6) %>% add_row(unmatched="",.before=9) %>%
+  add_row(unmatched="",.before=12) %>% add_row(unmatched="",.before=15) %>% add_row(unmatched="",.before=18) %>%
+  mutate_all(~replace(., is.na(.), ""))
+#write.csv(app_table1,"manuscript/tables/app_table1.csv")  
 
 
-mutate(cal=paste0(ifelse(calorie_mean>=100,sprintf('%.0f',calorie_mean),sprintf('%.2f',calorie_mean)),
-                  " (",ifelse(calorie_sd>=100,sprintf('%.0f',calorie_sd),sprintf('%.2f',calorie_sd)),")")) %>%
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
